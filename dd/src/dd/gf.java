c9f694637a445d41457c6ffd2fc58df183ac9b74
profesor <profesor@profesor-VirtualBox>
2020-02-17 09:53:07 +0100

    efqwerqs

---
+++ dd/src/dd/gf.java
@@ -0,0 +1,46 @@
+package dd;
+
+public class gf {
+	public static void main(String[] args) {
+		// Declaring a variable
+		int number = 2;
+		// Switch expression
+		switch (number) {
+		// Case statements
+		case 1:
+			System.out.println("Value 1");
+			break;
+		case 2: {
+			System.out.println("Value 2");
+			// Declaring a variable
+			int number2 = 3;
+			// Switch expression
+			switch (number2) {
+			// Case statements
+			case 1:
+				System.out.println("Value 1");
+				break;
+			case 2:
+				System.out.println("Value 2");
+				break;
+			case 3:
+				System.out.println("Value 3");
+				break;
+			// Default case statement
+			default:
+				System.out.println("Not in 1, 2 or 3");
+			}
+			break;
+		}
+		case 3:
+			System.out.println("Value 3");
+			break;
+		// Default case statement
+		default:
+			System.out.println("Not in 1, 2 or 3");
+			
+			
+			System.out.println("Mi nombre es Gonzalo");
+		}
+	}
+}
